4d24004f052e290cdb20315ca3c1afd8b1752506
YunHwan Nam <nanyounan@naver.com>
2016-12-13 16:35:08 +0900

    sort_test

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,20 @@ var Post = require('../models/Post');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   Post.find.sort({},{"meta.reservs": -1 },function(err, posts){
-//     res.render('index', {
-//       posts: posts
-//     });
-//   }).limit(3);
-// });
-
-
 router.get('/', function(req, res, next) {
-  Post.find({}).sort({"meta.reservs": -1}).exec(function(err, posts){
+  Post.find.sort({},{"meta.reservs": -1 },function(err, posts){
     res.render('index', {
       posts: posts
     });
   }).limit(3);
 });
+
+
+// router.get('/', function(req, res, next) {
+//   Post.find({}).sort({"meta.reservs": -1}).exec(function(err, posts){
+//     res.render('index', {
+//       posts: posts
+//     });
+//   }).limit(3);
+// });
 module.exports = router;
